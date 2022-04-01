2e4859f96c1cc9c026bace157d82be5ad57a8efd
15217283411 <1160886967@qq.com>
2022-04-01 15:05:30 +0000

    删除文件 crop-multi-api

--- crop-multi-api/crop-multi-mapper/src/main/java/com.crop.mapper/ArticleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.crop.mapper;
-
-import com.crop.pojo.Article;
-import com.crop.utils.MyMapper;
-
-public interface ArticleMapper extends MyMapper<Article> {
-}
--- crop-multi-api/crop-multi-mapper/src/main/java/com.crop.mapper/Articles2tagsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.crop.mapper;
-
-import com.crop.pojo.Articles2tags;
-import com.crop.utils.MyMapper;
-
-public interface Articles2tagsMapper extends MyMapper<Articles2tags> {
-}
