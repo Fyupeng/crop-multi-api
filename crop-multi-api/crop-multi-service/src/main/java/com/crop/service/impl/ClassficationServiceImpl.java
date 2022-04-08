562e31af987ae51e5581544407ac75ea5c4d4902
15217283411 <1160886967@qq.com>
2022-04-08 04:05:52 +0000

    更新和删除操作更改为管理员权限

--- crop-multi-api/crop-multi-service/src/main/java/com/crop/service/impl/ClassficationServiceImpl.java
+++ crop-multi-api/crop-multi-service/src/main/java/com/crop/service/impl/ClassficationServiceImpl.java
@@ -6,8 +6,12 @@ import com.crop.service.ClassficationService;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.entity.Example.Criteria;
+
+import java.util.List;
 
 /**
  * @Auther: fyp
@@ -27,6 +31,7 @@ public class ClassficationServiceImpl implements ClassficationService {
     private Sid sid;
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
     public boolean queryClassficationIdIsExist(String classId) {
 
         Classfication classfication = classficationMapper.selectByPrimaryKey(classId);
@@ -34,7 +39,13 @@ public class ClassficationServiceImpl implements ClassficationService {
         return classfication == null ? false : true;
     }
 
+    /**
+     * 当前没有事务，以 无事务方式 执行，有 事务，多个查询线程 使用同一个 事务
+     * @param classfication
+     * @return
+     */
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
     public Classfication queryClssfication(Classfication classfication) {
 
         Example classficationExample = new Example(Classfication.class);
@@ -47,6 +58,24 @@ public class ClassficationServiceImpl implements ClassficationService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public List<Classfication> queryAllClassfications() {
+
+        Example classficationExample = new Example(Classfication.class);
+        Criteria criteria = classficationExample.createCriteria();
+
+        List<Classfication> result = classficationMapper.selectAll();
+
+        return result;
+    }
+
+
+    /**
+     * 当前没有其他事务，新建事务，这样该方法 调用多次 实际上是 多个事务，体现原子性
+     * @param classfication
+     */
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public void saveClassfication(Classfication classfication) {
 
         String classficationId = sid.nextShort();
@@ -56,6 +85,31 @@ public class ClassficationServiceImpl implements ClassficationService {
         classficationMapper.insert(classfication);
     }
 
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public boolean updateClassfication(Classfication clssfication) {
+
+        int i = classficationMapper.updateByPrimaryKeySelective(clssfication);
+
+        return i > 0 ? true : false;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public boolean deleteClassfication(String classficationId) {
+
+        int i = classficationMapper.deleteByPrimaryKey(classficationId);
+
+        return i > 0 ? true : false;
+    }
+
 
 
 }
+
+
+
+
+
+
+
