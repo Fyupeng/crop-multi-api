efa8e9eb43a1f14a9cc66c29846a008ad43f1aa4
fyp <1160886967@qq.com>
2022-04-21 22:10:18 +0800

    使用自定义bean来添加跨域

--- crop-multi-web/src/main/java/com/crop/config/WebMvcConfig.java
+++ crop-multi-web/src/main/java/com/crop/config/WebMvcConfig.java
@@ -2,11 +2,13 @@ package com.crop.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -15,6 +17,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
                 .addResourceLocations("file:" + System.getProperties().getProperty("user.home") + "/"
                 + "webapps" + "/" + "crop_multi_data/");
     }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+
+
 
 
 }
